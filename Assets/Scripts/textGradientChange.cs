51890e7d5c5c23c05afa9efdb4d9cd7365cf5f9e
Tony Doan <todoan@ucdavis.edu>
2019-06-02 13:47:03 -0700

    main menu

---
+++ Assets/Scripts/textGradientChange.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+using TMPro;
+
+public class textGradientChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler
+{
+    public TextMeshProUGUI txt; 
+  
+    void Start()
+    {
+        txt = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // When highlighted
+        txt.isRightToLeftText = true;
+        Debug.Log("im in");
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // When unhighlighted
+        txt.isRightToLeftText = false;
+        Debug.Log("im out");
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        // When selected -- For you, input man 
+    }
+}
