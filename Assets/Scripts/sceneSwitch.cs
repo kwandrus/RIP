604b967106876a6b429398f2e937a0b0c83b9590
Tony Doan <todoan@ucdavis.edu>
2019-06-02 20:57:30 -0700

    Barebone Scenes, Alc/Cig End

---
+++ Assets/Scripts/sceneSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class sceneSwitch : MonoBehaviour, ISelectHandler
+{
+    public string loadLevel; 
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        SceneManager.LoadScene(loadLevel);
+    }
+}
