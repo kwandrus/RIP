51cda848bb5cb1839634cea3ed9f2924fd75eb6d
Caleb Bolton <calbolt22@gmail.com>
2019-06-05 20:28:57 -0700

    MainMenu Functionality

---
+++ Assets/Scripts/ICommand.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommandPattern
+{
+
+    public interface ICommand
+    {
+        void Execute(GameObject gameObject);
+    }
+}
