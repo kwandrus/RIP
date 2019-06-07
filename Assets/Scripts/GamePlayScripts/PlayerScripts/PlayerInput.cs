e514282a0f366c60c1ce0386b6596f7686bcc120
Caleb Bolton <calbolt22@gmail.com>
2019-06-06 17:17:45 -0700

    Added player with Input, Movement, and Animation scripts

---
+++ Assets/Scripts/GamePlayScripts/PlayerScripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement playerMovement = gameObject.GetComponent<PlayerMovement>();
+        PlayerAnimation playerAnimation = gameObject.GetComponent<PlayerAnimation>();
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerMovement.Jump();
+            playerAnimation.Jump();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerMovement.MoveLeft();
+            playerAnimation.MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Crouch?
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerMovement.MoveRight();
+            playerAnimation.MoveRight();
+        }
+    }
+}
