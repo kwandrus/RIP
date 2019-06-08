8c4f1bec46b83bd97dd46a6df0836c4a79c5a9ef
Andrew Tsung <aktsung@ucdavis.edu>
2019-06-07 20:06:31 -0700

    cutscene + lose buttons

---
+++ Assets/Scripts/gameOverScripts/LoseButtonsController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using CommandPattern;
+
+public class LoseButtonsController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject RestartButton;
+    [SerializeField]
+    public GameObject CreditsButton;
+    [SerializeField]
+    GameObject Camera;
+    [SerializeField]
+    Light SceneLight;
+    enum State { Idle, Animation, Switch };
+    private State CurrentState = State.Idle;
+    private float AnimationTimer = 0.0f;
+    private readonly float AnimationDuration = 0.5f;
+    string SceneToLoad = "";    // No Generic Lose Scene.
+
+    private void Update()
+    {
+        if (CurrentState == State.Animation)
+        {
+            AnimationTimer += Time.deltaTime;
+
+            UpdateAnimation();
+
+            if (AnimationTimer > AnimationDuration)
+            {
+                CurrentState = State.Switch;
+            }
+        }
+        else if (CurrentState == State.Switch)
+        {
+            // Switch state in case we have to add code here later.
+            Debug.Log("Loading " + SceneToLoad);
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+
+    public void SwitchToGamePlay()
+    {
+        CurrentState = State.Animation;
+        DisableButtons();
+        SceneToLoad = "GamePlay";
+    }
+
+    public void SwitchToCredits()
+    {
+        CurrentState = State.Animation;
+        DisableButtons();
+        SceneToLoad = "Credits";
+    }
+
+    private void UpdateAnimation()
+    {
+        Debug.Log("Updating Animation");
+
+        float buttonMovementSpeed = 2000;
+        float distanceToMove = Time.deltaTime * buttonMovementSpeed;
+        Vector2 buttonMovementVectorLeft = new Vector2(-distanceToMove, 0);
+        Vector2 buttonMovementVectorRight = new Vector2(distanceToMove, 0);
+
+        RestartButton.transform.Translate(buttonMovementVectorLeft);
+        CreditsButton.transform.Translate(buttonMovementVectorRight);
+    }
+
+    private void DisableButtons()
+    {
+        RestartButton.GetComponent<Button>().interactable = false;
+        CreditsButton.GetComponent<Button>().interactable = false;
+    }
+}
