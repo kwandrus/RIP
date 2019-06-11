b1704bf2fc355d4a4c51ae5baaac4610959b974f
Caleb Bolton <calbolt22@gmail.com>
2019-06-10 20:53:51 -0700

    Added screen shake upon falling out of world

---
+++ Assets/Scripts/GamePlayScripts/ScreenShakeEffect.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakeEffect : MonoBehaviour
+{
+    [SerializeField]
+    private float Frequency;
+    private float FrequencyTimer = 0.0f;
+    [SerializeField]
+    private float Duration;
+    private float Timer = 0.0f;
+
+    [SerializeField]
+    private AnimationCurve Curve;
+
+    private bool Active = false;
+    private Vector3 InitialPosition;
+
+    void Update()
+    {
+        if (Active)
+        {
+            Timer += Time.deltaTime;
+            if (Timer <= Duration)
+            {
+                gameObject.transform.localPosition = InitialPosition + Random.insideUnitSphere * Curve.Evaluate(Timer/Duration);
+                Debug.Log("Initial position: " + InitialPosition);
+                Debug.Log("New location: " + gameObject.transform.position);
+            }
+            else
+            {
+                Timer = 0f;
+                gameObject.transform.localPosition = InitialPosition;
+                Active = false;
+            }
+        }
+    }
+
+    public void Activate()
+    {
+        InitialPosition = gameObject.transform.localPosition;
+        Debug.Log("Activate; Initial Position: " + InitialPosition);
+        Active = true;
+    }
+}
