b3a0b3cbf5c95f7db07f81f537824bb5d6873d5b
Kyle Andrus <kwandrus@ucdavis.edu>
2019-06-10 20:20:02 -0700

    added song to cutscene

---
+++ Assets/Scripts/AudioScripts/CutSceneAudio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource cutSceneSong;
+
+    public void Start()
+    {
+        cutSceneSong.Play();
+    }
+}
---
+++ Assets/Scripts/AudioScripts/PlayerAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource jumpUpSound;
+    [SerializeField] private AudioSource jumpLandSound;
+    [SerializeField] private AudioSource deathGruntSound;
+
+    public void JumpUp()
+    {
+        jumpUpSound.Play();
+    }
+
+    public void JumpLand()
+    {
+        jumpLandSound.Play();
+    }
+
+    public void DeathGrunt()
+    {
+        deathGruntSound.Play();
+    }
+}
