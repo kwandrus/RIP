dd10e1a0242695401fc9846f8570649434f8ba4e
CalBolt <calbolt22@gmail.com>
2019-06-11 04:20:52 -0700

    Added PlayerCollision script

---
+++ Assets/Scripts/GamePlayScripts/PlayerScripts/PlayerCollision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GamePlay.Player
+{
+    public class PlayerCollision : MonoBehaviour
+    {
+        // Publishers.
+        public delegate void CollideWithCigarette(Transform cigarette);
+        public static event CollideWithCigarette OnCollideWithCigarette;
+        public delegate void CollideWithAlcohol(Transform alcohol);
+        public static event CollideWithAlcohol OnCollideWithAlcohol;
+        public delegate void CollideWithCheckpoint(Vector2 position);
+        public static event CollideWithCheckpoint OnCollideWithCheckpoint;
+        public delegate void CollideWithHostile();
+        public static event CollideWithHostile OnCollideWithHostile;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.transform.tag == "Cigarette")
+            {
+                OnCollideWithCigarette(collision.transform);
+            }
+            if (collision.transform.tag == "Alcohol")
+            {
+                OnCollideWithAlcohol(collision.transform);
+            }
+            if (collision.transform.tag == "Checkpoint")
+            {
+                OnCollideWithCheckpoint(collision.transform.position);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.transform.tag == "Hostile")
+            {
+                OnCollideWithHostile();
+            }
+        }
+    }
+}
